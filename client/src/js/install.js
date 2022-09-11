183c7688234f2183b4f12310fa9960c07ad009b0
Zchun <z.c.chun2003@gmail.com>
2022-09-11 13:29:05 -0500

    install logic

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,25 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
 
-butInstall.addEventListener('click', async () => {});
+  butInstall.classList.toggle('hidden', false);
+});
 
-window.addEventListener('appinstalled', (event) => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+});
+
+window.addEventListener('appinstalled', (event) => {
+    if (!promptEvent) {
+        return;
+      }
+      promptEvent.prompt();
+      window.deferredPrompt = null;
+      butInstall.classList.toggle('hidden', true);
+    });
+    
+    window.addEventListener('appinstalled', (event) => {
+      window.deferredPrompt = null;
+});
